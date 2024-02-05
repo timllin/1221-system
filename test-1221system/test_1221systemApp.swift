76951b810a10d33d5f2f99a08c3614dad478af3f
Тимур Калимуллин <kalimullin.tt@phystech.edu>
2024-02-05 12:06:12 +0300

    Initial Commit

---
+++ test-1221system/test_1221systemApp.swift
@@ -0,0 +1,17 @@
+//
+//  test_1221systemApp.swift
+//  test-1221system
+//
+//  Created by Тимур Калимуллин on 05.02.2024.
+//
+
+import SwiftUI
+
+@main
+struct test_1221systemApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
